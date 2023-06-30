a8151d9b2240177883ee1e498ac5819096ee42e1
Administrator <Administrator@DESKTOP-DA7JM6Q>
2023-06-30 09:20:58 +0800

    添加项目文件。

---
+++ RoslynHelperApi/Program.cs
@@ -0,0 +1,53 @@
+using MirrorSharp.AspNetCore;
+using MonacoRoslynCompletionProvider;
+using MonacoRoslynCompletionProvider.Api;
+using System.Reflection;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options => {
+	options.AddPolicy("all", builder => {
+		builder.AllowAnyHeader()
+		.AllowAnyOrigin()
+		.AllowAnyMethod();
+	});
+});
+// Add services to the container.
+
+var app = builder.Build();
+app.UseCors("all");
+app.MapPost("/completion/{0}", async (e) => {
+	using var reader = new StreamReader(e.Request.Body);
+	string text = await reader.ReadToEndAsync();
+	if (text != null) {
+		if (e.Request.Path.Value?.EndsWith("complete") == true) {
+			var tabCompletionRequest = JsonSerializer.Deserialize<TabCompletionRequest>(text);
+			var tabCompletionResults = await CompletitionRequestHandler.Handle(tabCompletionRequest);
+			await JsonSerializer.SerializeAsync(e.Response.Body, tabCompletionResults);
+			return;
+		}
+		else if (e.Request.Path.Value?.EndsWith("signature") == true) {
+			var signatureHelpRequest = JsonSerializer.Deserialize<SignatureHelpRequest>(text);
+			var signatureHelpResult = await CompletitionRequestHandler.Handle(signatureHelpRequest);
+			await JsonSerializer.SerializeAsync(e.Response.Body, signatureHelpResult);
+			return;
+		}
+		else if (e.Request.Path.Value?.EndsWith("hover") == true) {
+			var hoverInfoRequest = JsonSerializer.Deserialize<HoverInfoRequest>(text);
+			var hoverInfoResult = await CompletitionRequestHandler.Handle(hoverInfoRequest);
+			await JsonSerializer.SerializeAsync(e.Response.Body, hoverInfoResult);
+			return;
+		}
+		else if (e.Request.Path.Value?.EndsWith("codeCheck") == true) {
+			var codeCheckRequest = JsonSerializer.Deserialize<CodeCheckRequest>(text);
+			var codeCheckResults = await CompletitionRequestHandler.Handle(codeCheckRequest);
+			await JsonSerializer.SerializeAsync(e.Response.Body, codeCheckResults);
+			return;
+		}
+	}
+
+	e.Response.StatusCode = 405;
+});
+app.MapMirrorSharp("/mirrosharp");
+app.Run();
---
+++ StringCompiler/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+using System.Text;
+
+string str = @"
+using System;
+namespace Test {
+  public class Student {
+     public void Run() {
+      Console.WriteLine(123456);
+     }
+  }
+}
+";
+
+SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(str, encoding: Encoding.UTF8);
+CSharpCompilation compilation = CSharpCompilation.Create("StringCompiler",
+    syntaxTrees: new[] { syntaxTree },
+    references: new[] {
+       MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+       MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Console")).Location),
+       MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Runtime")).Location)
+    },
+    options:new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+using var memory = new MemoryStream();
+EmitResult result = compilation.Emit(memory);
+foreach (var item in result.Diagnostics) {
+    Console.WriteLine(item.ToString());
+}
+if (!result.Success) return;
+memory.Seek(0, SeekOrigin.Begin);
+Assembly assembly = Assembly.Load(memory.ToArray());
+Type type = assembly.GetType("Test.Student") ?? throw new Exception("未找到Student类型");
+MethodInfo method =  type.GetMethod("Run", BindingFlags.Public | BindingFlags.Instance) ?? throw new Exception("未找到Run方法");
+object obj = Activator.CreateInstance(type);
+method.Invoke(obj, null); 
+
+
+CompilationUnitSyntax root = syntaxTree.GetCompilationUnitRoot();
+Console.WriteLine($"The tree is a {root.Kind()} node.");
+Console.WriteLine($"The tree has {root.Members.Count} elements in it.");
+Console.WriteLine($"The tree has {root.Usings.Count} using statements. They are:");
+foreach (UsingDirectiveSyntax element in root.Usings)
+    Console.WriteLine($"\t{element.Name}");
+
+MemberDeclarationSyntax firstMember = root.Members[0];
+Console.WriteLine($"The first member is a {firstMember.Kind()}.");
+
